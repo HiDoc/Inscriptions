d2676377693876a82f5a2a49ae04d4f285bdc250
HiDoc <sound6tem@hotmail.co.uk>
2017-01-20 12:09:30 +0100

    Constructeur de query

---
+++ src/data/database/Queries.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.database;
+
+/**
+ *
+ * @author Flo
+ */
+public class Queries {
+    private String mQuery;
+    private String type;
+    private String table;
+    
+    public void Queries(String table, String type){
+        this.type = type;
+        this.table = table;
+    }
+    public void buildQuery(){
+        switch(this.type){
+            case "insert":
+                this.mQuery = "insert into ";
+                break;
+            case "select":
+                this.mQuery = "select * from ";
+                break;
+            case "update":
+                this.mQuery = "update ";
+                break;
+            default : 
+                this.mQuery = "";
+        }
+        this.mQuery += table;
+    }
+    public String getMQuery(){
+        return this.mQuery;
+    }
+}
