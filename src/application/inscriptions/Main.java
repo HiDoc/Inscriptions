4b84926150c225e391798fc2f96fac9df0f64dd9
georges <gderemusat@gmail.com>
2017-01-20 11:57:23 +0100

    -

--- src/application/inscriptions/Main.java
+++ src/application/inscriptions/Main.java
@@ -1,10 +1,12 @@
 package application.inscriptions;
 
+import presentation.cli.menu.MainMenu;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MainMenu mainMenu = new MainMenu(Inscriptions.getInscriptions());
 	}
 
 }
